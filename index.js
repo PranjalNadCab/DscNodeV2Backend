6e4cb1be69774a897fda91a7189f1929ed570524
Shivam <shivamnadcab@gmail.com>
2025-10-03 17:37:14 +0530

    working

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const server = app.listen(PORT, async () => {
         const res = await getLivePrice();
         console.log("Live DSC Price fetched successfully:", res);
         await generateDefaultAdminDoc();
-        // await dscNodeListEvents();
+        await dscNodeListEvents();
     }
 });
 
