98c4656f70d48efb5ebe2dbdf884765b664078ed
Shivam <shivamnadcab@gmail.com>
2025-10-08 12:50:33 +0530

    removed unwanted events from indexer and managed indexer code

--- src/indexer/nodeIndexer.js
+++ src/indexer/nodeIndexer.js
@@ -76,218 +76,20 @@ async function processEvents(events) {
                 }
 
             }
-            // else if (event == "Staked") {
-            //     try {
+            else if(event == "NbdPaid"){
+                try{
+                    let {userAddress,majorIncome,minor4Income,amountNbdPaid} = returnValues;
 
-            //         let { userAddress, amount, totalAmountInUsd, currency, rateDollarPerDsc, lastUsedNonce, mixTxHash } = returnValues;
+                    amountNbdPaid = new BigNumber(amountNbdPaid).toFixed();
 
-            //         amount = new BigNumber(amount).toFixed();
-            //         totalAmountInUsd = new BigNumber(totalAmountInUsd).toFixed();
-            //         lastUsedNonce = Number(lastUsedNonce);
-            //         rateDollarPerDsc = new BigNumber(rateDollarPerDsc).toFixed();
-
-            //         // const totalUsd = new BigNumber(amountDscInUsd).plus(amountUsdt).toFixed();
-            //         let isPendingStake = false;
-            //         if (mixTxHash == zeroAddressTxhash) {
-            //             isPendingStake = true;
-            //             mixTxHash = transactionHash
-            //         } else if ((mixTxHash !== "NA") && (mixTxHash !== zeroAddressTxhash)) {
-            //             const userPendingStakes = await StakingModel.find({ userAddress: userAddress, isPendingStake: true, mixTxHash: mixTxHash });
-            //             let amountUsdPaidForDsc = userPendingStakes.filter((stake) => stake.currency === "DSC").reduce((sum, item) => {
-            //                 return sum.plus(item.amountUsdPaid)
-            //             }, new BigNumber(0));
-            //             amountUsdPaidForDsc = amountUsdPaidForDsc.plus(amount);
-            //             const userUsdtStakePart = userPendingStakes.find((item) => item.currency === "USDT");
-            //             const remainingUsdToPay = new BigNumber(userUsdtStakePart.totalAmountInUsd).minus(amountUsdPaidForDsc).minus(userUsdtStakePart.amountUsdPaid);
-            //             isPendingStake = remainingUsdToPay.isEqualTo(0) ? false : true;
-
-            //         }
-
-            //         let amountInUsdt = "0";
-            //         let amountInDscInUsd = "0";
-            //         let amountDsc = "0"
-            //         if (currency === "USDT") {
-            //             amountInUsdt = amount;
-            //         }
-            //         else {
-            //             amountInDscInUsd = amount;
-            //             amountDsc = new BigNumber(amount).dividedBy(rateDollarPerDsc).multipliedBy(1e18).toFixed(0);
-            //         }
+                    await sendNodeRegIncomeToUpline(userAddress,majorIncome,minor4Income,Number(timestampNormal),amountNbdPaid);
 
 
-
-            //         const newStake = await StakingModel.create({
-            //             userAddress,
-            //             currency,
-            //             totalAmountInUsd: totalAmountInUsd,
-            //             amountInDscInUsd: amountInDscInUsd,
-            //             amountUsdPaid: amount,
-            //             amountInDsc: amountDsc,
-            //             amountInUsdt: amountInUsdt,
-            //             rateDollarPerDsc: rateDollarPerDsc,
-            //             time: Number(timestampNormal),
-            //             lastUsedNonce,
-            //             block: Number(block),
-            //             transactionHash: transactionHash,
-            //             mixTxHash: mixTxHash,
-            //             isPendingStake,
-            //         });
-
-            //         console.log("New stake created:", newStake);
-
-
-
-            //         let rankDuringStaking = null;
-            //         const userDoc = await RegistrationModel.findOne({ userAddress: userAddress });
-
-            //         rankDuringStaking = userDoc.currentRank;
-
-            //         await updateUserTotalSelfStakeUsdt(userAddress, amount);
-            //         await updateDirectBusiness(amount, userAddress);
-            //         await manageRank(userAddress);
-            //         let rateDollarPerDscInNum = Number(new BigNumber(rateDollarPerDsc).dividedBy(1e18).toFixed(2));
-            //         if (!isPendingStake && mixTxHash !== "NA") {
-            //             const userTotalStakes = await StakingModel.find({ userAddress: userAddress, mixTxHash: mixTxHash });
-            //             const stakingAmountIn1e18 = userTotalStakes.find((item) => { return item.currency === "USDT" }).totalAmountInUsd;
-            //             const usdtStakedIn1e18 = userTotalStakes.find((item) => { return item.currency === "USDT" }).amountUsdPaid;
-            //             const dscStakedInUsdtIn1e18 = userTotalStakes.filter((item) => item.currency === "DSC").reduce((sum, item) => {
-            //                 return sum.plus(item.amountUsdPaid)
-            //             }, new BigNumber(0));
-
-            //             await giveGapIncome(userAddress, stakingAmountIn1e18, rankDuringStaking, usdtStakedIn1e18, dscStakedInUsdtIn1e18.toFixed(), "stake", rateDollarPerDscInNum);
-            //             await StakingModel.updateMany(
-            //                 { userAddress, mixTxHash },
-            //                 { $set: { isPendingStake: false } }
-            //             );
-            //         } else if (mixTxHash === "NA") {
-            //             await giveGapIncome(userAddress, totalAmountInUsd, rankDuringStaking, amountInUsdt, amountInDscInUsd, "stake", rateDollarPerDscInNum);
-
-            //         } else {
-            //             console.log("do nothing for incomeplete stakes");
-            //         }
-
-
-            //     } catch (error) {
-            //         console.log(error);
-            //         continue;
-            //     }
-            // }
-            else if (event == "WithdrawIncomeUsdt") {
-                try {
-                    const { userAddress, amountUsdt, amountUsdtAfterDeduction, lastUsedNonce } = returnValues;
-
-                    const newWithdraw = await WithdrawIncomeModel.create({
-                        userAddress,
-                        amountInUsdt: new BigNumber(amountUsdt).toFixed(),
-                        amountInUsdtAfterDeduction: new BigNumber(amountUsdtAfterDeduction).toFixed(),
-                        amountInDsc: null,
-                        amountInDscAfterDeduction: null,
-                        amountInDscInUsd: null,
-                        amountInDscInUsdAfterDeduction: null,
-                        time: Number(timestampNormal),
-                        lastUsedNonce: Number(lastUsedNonce),
-                        block: Number(block),
-                        transactionHash: transactionHash
-                    });
-
-                    console.log("Usdt withdraw doc created:", newWithdraw);
-
-                    await manageUserWallet(userAddress, new BigNumber(amountUsdt).toFixed(), null);
-
-                } catch (error) {
+                }catch(error){
                     console.log(error);
                     continue;
                 }
-            }
-            else if (event == "WithdrawIncomeDsc") {
-                try {
-                    const { amountDsc, amountDscInUsd, amountDscAfterDeduction, amountDscInUsdAfterDeduction, rateDollarPerDsc, userAddress, lastUsedNonce } = returnValues;
-
-                    const newWithdraw = await WithdrawIncomeModel.create({
-                        userAddress,
-                        amountInUsdt: null,
-                        amountInUsdtAfterDeduction: null,
-                        amountInDsc: new BigNumber(amountDsc).toFixed(),
-                        amountInDscAfterDeduction: new BigNumber(amountDscAfterDeduction).toFixed(),
-                        amountInDscInUsd: new BigNumber(amountDscInUsd).toFixed(),
-                        amountInDscInUsdAfterDeduction: new BigNumber(amountDscInUsdAfterDeduction).toFixed(),
-                        time: Number(timestampNormal),
-                        lastUsedNonce: Number(lastUsedNonce),
-                        block: Number(block),
-                        transactionHash: transactionHash
-                    });
-
-                    console.log("Dsc withdraw doc created:", newWithdraw);
-
-                    await manageUserWallet(userAddress, null, new BigNumber(amountDsc).toFixed());
-
-
-                } catch (error) {
-                    console.log(error);
-                    continue;
-                }
-            }
-            else if (event == "NodeDeployed") {
-                try {
-                    const { user, nodeNum } = returnValues;
-
-                    const nodeConverted = await NodeDeployedModel.create({
-                        userAddress: user,
-                        nodeNum: Number(nodeNum),
-                        time: Number(timestampNormal),
-                        block: Number(block),
-                        transactionHash: transactionHash,
-                        lastRoiDistributed: moment().startOf('day').unix()
-                    });
-
-                    console.log("Node deployed-->>", nodeConverted);
-
-                    await updateUserNodeInfo(user, Number(nodeNum), Number(timestampNormal));
-
-
-                } catch (error) {
-                    console.log(error);
-                    continue;
-                }
-            }
-            // else if (event == "NodeRegistered") {
-            //     try {
-            //         let { user, amountUsdtPaid, majorIncome, minor4Income } = returnValues;
-            //         amountUsdtPaid = new BigNumber(amountUsdtPaid).toFixed(0);
-            //         majorIncome = new BigNumber(majorIncome).toFixed(0);
-            //         minor4Income = new BigNumber(minor4Income).toFixed(0);
-
-            //         const newReg = await NodesRegistered.create({
-            //             userAddress: user,
-            //             amountUsdtPaid,
-            //             majorIncome,
-            //             minor4Income,
-            //             time: Number(timestampNormal),
-            //             block: Number(block),
-            //             transactionHash: transactionHash
-            //         });
-            //         console.log("New node registered", newReg)
-
-            //         const regDoc = await RegistrationModel.findOne({ userAddress: user });
-            //         if (!regDoc) {
-            //             console.log("No registration doc found for user while registering node:", user);
-            //         }
-            //         const updatedBalance = new BigNumber(regDoc.nodePurchasingBalance).plus(amountUsdtPaid).toFixed(0);
-
-            //         regDoc.nodePurchasingBalance = updatedBalance;
-            //         regDoc.isNodeRegDone = true;
-            //         await regDoc.save();
-
-            //         await sendNodeRegIncomeToUpline(user, majorIncome, minor4Income, Number(timestampNormal));
-
-            //     } catch (error) {
-            //         console.log(error);
-            //         continue;
-            //     }
-
-
-
-            // }
+            }   
             else if (event == "UpgradeNode") {
                 try {
                     let { user, nodeNum, amount, lastUsedNonce, totalAmountInUsd, mixTxHash, currency, rate } = returnValues;
@@ -397,16 +199,80 @@ async function processEvents(events) {
                 }
 
             }
-            else if(event == "NbdPaid"){
-                try{
-                    let {userAddress,majorIncome,minor4Income,amountNbdPaid} = returnValues;
+            else if (event == "NodeDeployed") {
+                try {
+                    const { user, nodeNum } = returnValues;
 
-                    amountNbdPaid = new BigNumber(amountNbdPaid).toFixed();
+                    const nodeConverted = await NodeDeployedModel.create({
+                        userAddress: user,
+                        nodeNum: Number(nodeNum),
+                        time: Number(timestampNormal),
+                        block: Number(block),
+                        transactionHash: transactionHash,
+                        lastRoiDistributed: moment().startOf('day').unix()
+                    });
 
-                    await sendNodeRegIncomeToUpline(userAddress,majorIncome,minor4Income,Number(timestampNormal),amountNbdPaid);
+                    console.log("Node deployed-->>", nodeConverted);
+
+                    await updateUserNodeInfo(user, Number(nodeNum), Number(timestampNormal));
 
 
-                }catch(error){
+                } catch (error) {
+                    console.log(error);
+                    continue;
+                }
+            }
+            else if (event == "WithdrawIncomeUsdt") {
+                try {
+                    const { userAddress, amountUsdt, amountUsdtAfterDeduction, lastUsedNonce } = returnValues;
+
+                    const newWithdraw = await WithdrawIncomeModel.create({
+                        userAddress,
+                        amountInUsdt: new BigNumber(amountUsdt).toFixed(),
+                        amountInUsdtAfterDeduction: new BigNumber(amountUsdtAfterDeduction).toFixed(),
+                        amountInDsc: null,
+                        amountInDscAfterDeduction: null,
+                        amountInDscInUsd: null,
+                        amountInDscInUsdAfterDeduction: null,
+                        time: Number(timestampNormal),
+                        lastUsedNonce: Number(lastUsedNonce),
+                        block: Number(block),
+                        transactionHash: transactionHash
+                    });
+
+                    console.log("Usdt withdraw doc created:", newWithdraw);
+
+                    await manageUserWallet(userAddress, new BigNumber(amountUsdt).toFixed(), null);
+
+                } catch (error) {
+                    console.log(error);
+                    continue;
+                }
+            }
+            else if (event == "WithdrawIncomeDsc") {
+                try {
+                    const { amountDsc, amountDscInUsd, amountDscAfterDeduction, amountDscInUsdAfterDeduction, rateDollarPerDsc, userAddress, lastUsedNonce } = returnValues;
+
+                    const newWithdraw = await WithdrawIncomeModel.create({
+                        userAddress,
+                        amountInUsdt: null,
+                        amountInUsdtAfterDeduction: null,
+                        amountInDsc: new BigNumber(amountDsc).toFixed(),
+                        amountInDscAfterDeduction: new BigNumber(amountDscAfterDeduction).toFixed(),
+                        amountInDscInUsd: new BigNumber(amountDscInUsd).toFixed(),
+                        amountInDscInUsdAfterDeduction: new BigNumber(amountDscInUsdAfterDeduction).toFixed(),
+                        time: Number(timestampNormal),
+                        lastUsedNonce: Number(lastUsedNonce),
+                        block: Number(block),
+                        transactionHash: transactionHash
+                    });
+
+                    console.log("Dsc withdraw doc created:", newWithdraw);
+
+                    await manageUserWallet(userAddress, null, new BigNumber(amountDsc).toFixed());
+
+
+                } catch (error) {
                     console.log(error);
                     continue;
                 }
@@ -414,8 +280,6 @@ async function processEvents(events) {
             else {
                 console.log("Got no events!");
             }
-
-
         }
     } catch (error) {
         console.log(error)
