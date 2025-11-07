c004485e6eb1eb5850b1dfb150021e47e3cf3824
Shivam <shivamnadcab@gmail.com>
2025-11-07 11:10:21 +0530

    working

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ const server = app.listen(PORT, async () => {
         const res = await getLivePrice();
         console.log("Live DSC Price fetched successfully:", res);
         // await setLatestBlock();
-        // await dscNodeListEvents();
+        await dscNodeListEvents();
         // await generateDefaultAdminDoc();
         // await createDaoAndDelegatorsAdminInBulk();
         // await updateDaoDelegatorForAdmins();
