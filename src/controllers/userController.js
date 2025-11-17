3615f9a142b5546c08b114f0fe2607c986e0e38e
Shivam <shivamnadcab@gmail.com>
2025-11-17 17:39:26 +0530

    pushing code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -300,7 +300,7 @@ const withdrawIncomeUsdt = async (req, res, next) => {
     try {
         let { userAddress, amountUsdt } = req.body;
 
-        // throw new Error("Currently unavailable!");
+        throw new Error("Currently unavailable!");
 
         // ✅ Validate required fields
         const missingFields = Object.entries(req.body)
@@ -382,7 +382,7 @@ const withdrawIncomeDsc = async (req, res, next) => {
     try {
         let { userAddress, amountDscInUsd } = req.body;
 
-        // throw new Error("Currently unavailable!");
+        throw new Error("Currently unavailable!");
 
         // ✅ Validate required fields
         const missingFields = Object.entries(req.body)
